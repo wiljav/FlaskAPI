330bf728b5cf95c5e20e58d244ede3eb5cf0ccec
wiljav <wiljav@users.noreply.github.com>
2022-03-13 14:26:02 +0200

    adding new features

--- app.py
+++ app.py
@@ -1,59 +1,83 @@
-#!/usr/bin/env python
 
-# Installing flask using "pip install Flask" in Linux (Ubuntu)
-from flask import Flask, request, redirect, url_for, render_template, make_response
+from flask import Flask, render_template, jsonify, request
+from flask_restful import Resource, Api
 import collections
+import json
 
-# including the built-in __name__ variable inside Flask to evaluate the name of the module
 app = Flask(__name__)
+api = Api(app)
 
-# setting up a location to in the server to use the POST and GET methods.
-# According to the challenge this is "/analyze"
-@app.route('/analyze', methods=["POST", "GET"])
 
-class App():
-    def get():
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('./index.html'),200,headers)
+class status(Resource):
+    def get(self):
+        try:
+            return {'text': 'Api is Running'}
+        except:
+            return {'text': 'An Error Occurred during fetching Api'}
 
-    def main():
-        # when making the POST request, the following code will be running
-        if request.method == "POST":
-            # # pulling the text from the user input
-            # raw_text = request.get_json()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-            # # Using one line for loop to:
-            # # 1. Iterating through the values of the dictionary
-            # # 2. Stripping them from the brackets left by the list after converting the list to a string type
-            # text = str([x for x in raw_text.values()]).strip("[]'")
+@app.route('/processed',methods = ['POST'])
+def processed():
+    raw_text = dict(request.form)['raw_text']
+    
+    try:
+        text = json.loads(raw_text)['text']
+        text_length = {"withSpaces":len(text), "withoutSpaces":len(text.replace(' ',''))}
+        word_count = len(text.split())
+        char_count = dict(collections.Counter(text))
+        file = {'textLength':text_length, 'wordCount':word_count, 'characterCount':char_count}
+        return render_template("analyze.html",result = file)#file)
 
-            # # Counting the length of the "text" with and without spaces
-            # text_length = {"withSpaces":len(text), "withoutSpaces":len(text.replace(' ',''))}
+        # if string
+    except TypeError:
+        text = json.loads(str(raw_text))
+        text_length = {"withSpaces":len(text), "withoutSpaces":len(text.replace(' ',''))}
+        word_count = len(text.split())
+        char_count = dict(collections.Counter(text))
+        file = {'textLength':text_length, 'wordCount':word_count, 'characterCount':char_count}
 
-            # # Counting number of words inside in the "text"
-            # word_count = len(text.split())
+        return render_template("analyze.html",result = file)
 
-            # # Counting the number of characters in the "text"
-            # char_count = dict(collections.Counter(text))
-            # file = {'textLength':text_length, 'wordCount':word_count, 'characterCount':char_count}
-            # return file
+    except json.decoder.JSONDecodeError:
+        text = raw_text
+        text_length = {"withSpaces":len(text), "withoutSpaces":len(text.replace(' ',''))}
+        word_count = len(text.split())
+        char_count = dict(collections.Counter(text))
+        file = {'textLength':text_length, 'wordCount':word_count, 'characterCount':char_count}
 
-            return render_template('index.html', command=request.form['command'],
-                text=request.form['text'])
+        return render_template("analyze.html",result = file)
 
-        # When making a GET request this will be the response
-        elif request.method == "GET":
-            return get()#"This is a GET request"
+    else:
+        return "Unsupported data type"
 
-    ''' To run this file, simply install flask, the run the command:
-        flask run
-        Then access the following link from your browser to get the GET request msg:
-        127.0.0.1:5000
+@app.route('/analyze',methods = ['POST'])
+def analyze():
+    # pulling the text from the user input
+    raw_text = request.get_json()
 
-        OR
+    # Using one line for loop to:
+    # 1. Iterating through the values of the dictionary
+    # 2. Stripping them from the brackets left by the list after converting the list to a string type
+    text = str([x for x in raw_text.values()]).strip("[]'")
 
-        in your terminal put the following command to get the POST request msg:
-        curl -i -X POST -H "Content-Type: application/json" -d '{"text":"hello 2 times  "}' 127.0.0.1:5000/analyze
-    '''
+    # Counting the length of the "text" with and without spaces
+    text_length = {"withSpaces":len(text), "withoutSpaces":len(text.replace(' ',''))}
+
+    # Counting number of words inside in the "text"
+    word_count = len(text.split())
+
+    # Counting the number of characters in the "text"
+    char_count = dict(collections.Counter(text))
+    file = {'textLength':text_length, 'wordCount':word_count, 'characterCount':char_count}
+    return file
+
+
+
+
+api.add_resource(status, '/')
+
 if __name__ == '__main__':
-    App.app.run()#host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=True)
