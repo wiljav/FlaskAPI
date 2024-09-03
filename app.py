8d0a89d6474b88362acacc169ac70b41ec1987f6
William Jawad <29238875+wiljav@users.noreply.github.com>
2024-09-03 10:51:25 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,56 +1,42 @@
 
-from flask import Flask, render_template, jsonify, request
 import collections
 import json
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def textProcessing(text):
-
+def text_processing(text):
+    """processing the text and adding it to a hashtable"""
     # Counting the length of the "text" with and without spaces
     text_length = {"withSpaces":len(text), "withoutSpaces":len(text.replace(' ',''))}
-
     # Counting number of words inside in the "text"
     word_count = len(text.split())
-
     # Counting the number of characters in the "text"
     char_count = dict(collections.Counter(text))
     file = {'textLength':text_length,
             'wordCount':word_count,
             'characterCount':char_count}
     return file
-
 @app.route('/processed',methods = ['POST'])
 def processed():
+    """processing the hashtable"""
     raw_text = dict(request.form)['raw_text']
-    
     try:
         text = json.loads(raw_text)['text']
-        return render_template("analyze.html", result = textProcessing(text))
-
+        return render_template("analyze.html", result = text_processing(text))
     except TypeError:
         text = json.loads(str(raw_text))
-        return render_template("analyze.html", result = textProcessing(text))
-
+        return render_template("analyze.html", result = text_processing(text))
     except json.decoder.JSONDecodeError:
         text = raw_text
-        return render_template("analyze.html", result = textProcessing(text))
-
-    else:
-        return "Unsupported data type"
-
+        return render_template("analyze.html", result = text_processing(text))
 @app.route('/analyze',methods = ['POST'])
 def analyze():
-    # pulling the text from the user input
+    """pulling the text from the user input"""
     raw_text = request.get_json()
     text = dict(raw_text)["text"]
-
-    return textProcessing(text)
-
-
+    return text_processing(text)
 if __name__ == '__main__':
     app.run(debug=True)
